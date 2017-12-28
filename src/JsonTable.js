3d1054f7fa4ba234ef2924e4fa17cedeca6ad1c0
Josh Ghent <me@joshghent.com>
2017-12-28 14:53:08 +0000

    Added method to render table.

--- src/JsonTable.js
+++ src/JsonTable.js
@@ -10,32 +10,38 @@ class JsonTable extends Component {
 			columns: Object.keys(this.props.data[0])
 		};
 	}
+	createCell(row) {
+		let rowData = [];
+		Object.values(row).forEach((elem) => {
+			rowData.push(<td>{elem}</td>)
+		})
+
+		return rowData;
+	}
+	createTable() {
+		let table = [];
+
+		let columnHeaders = [];
+		this.state.columns.forEach((elem) => {
+			columnHeaders.push(<th>{elem}</th>);
+		});
+
+		table.push(<thead><tr>{columnHeaders}</tr></thead>);
+
+		let rowData = [];
+		this.state.data.map((row) => {
+			rowData.push(<tr key={row.Month}>{this.createCell(row)}</tr>);
+		});
+
+		table.push(<tbody>{rowData}</tbody>);
+
+		return table;
+	}
   render() {
     return (
       <div className="JsonTable">
 				<table>
-					<thead>
-						<tr>
-							{
-								this.state.columns.forEach((elem) => {
-									return <th>{elem}</th>
-								})
-							}
-						</tr>
-					</thead>
-					<tbody>
-						{
-							this.state.data.map((row) => {
-								return <tr key={row.Month}>
-									{
-										Object.values(row).forEach((elem) => {
-											return <td>{elem.toString()}</td>
-										})
-									}
-								</tr>
-							})
-						}
-					</tbody>
+					{this.createTable()}
 				</table>
 			</div>
     );
