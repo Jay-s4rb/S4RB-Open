afb76b30e4d91d42c55e5969c6fc506beea6a0ec
Wafiq Baugh <warren.baugh@connect-group.com>
2018-01-02 18:01:10 +0000

    accumilating quaterly cpmu

--- src/components/cpmu_table/Table.js
+++ src/components/cpmu_table/Table.js
@@ -3,17 +3,24 @@ import Data from '../table_data/Data';
 import { cpmuCalc as calc, formatDate as fmt } from '../../utils';
 
 const Table = ({period, toggle, data}) => {
-
 	const dataRows = data.map((obj, i) => {
-		const cpmuCol = (obj.Complaints && obj.UnitsSold) ? calc(obj.Complaints, obj.UnitsSold) : 0.00000;
-		const periodCol = (period === 'Month')? fmt(obj.Month) : obj.Quarter;
+		let cpmuCol, periodCol;
+		
+		if(period === 'Month'){
+			 cpmuCol = (obj.Complaints && obj.UnitsSold) ? calc(obj.Complaints, obj.UnitsSold) : 0.00000;
+			 periodCol = fmt(obj.Month);
+		}else{
+			cpmuCol = obj.Cmpu || 0.00000;
+			periodCol = obj.Quarter;
+		}
+
 		return <Data columns={[periodCol, cpmuCol ]}  key={i} />;
 	});
 
 	return (
 		<table>
 			<tr>
-				<th onClick={e => toggle()} >{period}</th>
+				<th onClick={e => toggle()} >{(period === 'Quarter')? `${period} (over total of years)` : period }</th>
 				<th>Cpmu</th>
 			</tr>
 			{dataRows}
--- src/components/portal/Portal.js
+++ src/components/portal/Portal.js
@@ -3,7 +3,8 @@ import 'milligram';
 import endpoints from '../../api';
 import { Component } from 'preact';
 import Table from '../cpmu_table/Table';
-
+import { cpmuCalc as calc } from '../../utils';
+import {groupBy} from 'lodash';
 
 export default
 class Portal extends Component {
@@ -32,19 +33,41 @@ class Portal extends Component {
   togglePeriod() {
     this.setState({
       period: (this.state.period === 'Month' )? 'Quarter' : 'Month',
-      dataToDisplay: (this.state.period === 'Month' )? this.groupQuarterly() : this.state.data
+      dataToDisplay: (this.state.period === 'Month' ) ? this.displayByQuarter() : this.state.data
     });
-    //console.log(this.state.period);
   }
 
   groupQuarterly() {
-    
+     const grp = groupBy(this.state.data, obj => {
+      return obj.Quarter;
+    });
+    return grp;
+  }
+
+  reduceQuartely(i){
+    let units = 0, comps = 0;
+    i.forEach((item) => {
+      units +=  item.UnitsSold;
+      comps += item.Complaints;
+    });
+    return(calc(comps, units));
+  }
+
+  displayByQuarter(){
+    const grouped = this.groupQuarterly();
+    const qrt = [
+      {Quarter: 1, Cmpu: this.reduceQuartely(grouped['1'])},
+      {Quarter:2, Cmpu: this.reduceQuartely(grouped['2'])},
+      {Quarter:3, Cmpu: this.reduceQuartely(grouped['3'])},
+      {Quarter:4, Cmpu: this.reduceQuartely(grouped['4'])}
+    ];
+    return qrt;
   }
 
 	render(props, state) {
 		return (
 			<div>
-				<Table period={state.period} toggle={this.togglePeriod} data={state.dataToDisplay} />
+				<Table period={state.period} toggle={this.togglePeriod} data={(state.dataToDisplay.length > 0 )? state.dataToDisplay : state.data} />
 			</div>
 		);
 	}
